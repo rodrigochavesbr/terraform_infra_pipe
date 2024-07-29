9f0805c2054f675d9859cf0344427ae4f73551e4
Rodrigo Chaves <rschavesbr@gmail.com>
2024-07-28 22:54:57 -0300

    First test

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "sa-east-s1"
+  region = "sa-east-1"
 }
 
 resource "aws_s3_bucket_website_configuration" "static_site_bucket" {
-  bucket = "static-site-${var.bucket_name}"
+  bucket = aws_s3_bucket.static_site_bucket.bucket
 
   index_document {
     suffix = "index.html"
@@ -15,24 +15,28 @@ resource "aws_s3_bucket_website_configuration" "static_site_bucket" {
 }
 
 resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
-  bucket = aws_s3_bucket.static_site_bucket.bucket.id
+  bucket = aws_s3_bucket.static_site_bucket.bucket
 
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
-  restrict_public_buckets  = false
+  restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_owership_controls" "static_site_bucket" {
-  bucket = aws_s3_bucket.static_site_bucket.bucket.id
+resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
+  bucket = aws_s3_bucket.static_site_bucket.bucket
 
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
+
 resource "aws_s3_acl" "static_site_bucket" {
+  bucket = aws_s3_bucket.static_site_bucket.bucket
+  acl    = "public-read"
+
   depends_on = [
     aws_s3_bucket_public_access_block.static_site_bucket,
-    aws_s3_bucket_owership_controls.static_site_bucket
+    aws_s3_bucket_ownership_controls.static_site_bucket
   ]
 }
